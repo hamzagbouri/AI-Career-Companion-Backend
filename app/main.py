cbe36f24d9f8ac8c8790ef6d86dfe27b5fd8c203
Hamza Gbouri <hamzagbouri@Hamzas-MacBook-Pro.local>
2026-03-13 12:05:32 +0000

    fix cors

--- app/main.py
+++ app/main.py
@@ -17,7 +17,9 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 # CORS configuration
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 app.add_middleware(
